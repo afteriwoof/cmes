7eaccbce7cfc1d88b1e2e4d1a6305593773107b0
Jason Byrne <jbyrne6@gmail.com>
2015-12-09 15:56:52 +0000

    Updating codes and plots in hicat.py hicact.py

--- hicact_plots.py
+++ hicact_plots.py
@@ -14,7 +14,7 @@ Calls   :       os, numpy, matplotlib, pandas
 		config, savefig, cmes
 Written :       Jason P Byrne, STFC/RAL Space, Dec 2015 (jason.byrne@stfc.ac.uk)
 Revisions:
-2015-12-08 JPB : 
+2015-12-09 JPB : Updates to the code for better plots.
 '''
 
 import os
@@ -26,27 +26,18 @@ import config
 from savefig import save
 
 from cmes import cdaw,hicact,hicat
-
 # Call the catalog functions:
-#df_cdaw = cdaw().convert_objects(convert_numeric=True)
 df_hicact_a = hicact('A').convert_objects(convert_numeric=True)
 df_hicact_b = hicact('B').convert_objects(convert_numeric=True)
-#df_hicat = hicat().convert_objects(convert_numeric=True)
-
-# Drop NaNs
-# df.dropna(axis='rows',how='any',inplace=True)
-# df_cdaw.describe()
-# Generate some initial plots for CDAW
-# df_cdaw.hist()
-# save(path=os.path.join(config.wp3_path,"cdaw_cme_catalog/cdaw_hist"),verbose=True)
-# plt.show()
 
 # global variables
 binwidth=50 
 colors = ['r','b','grey']
 labels = ['Ahead','Behind','']
-speeds_xlim = [0,2100]
-speeds_xlabel = "Speed ($km s^{-1}$)"
+speeds_lim = [0,2100]
+speeds_label = "Speed ($km s^{-1}$)"
+ledge_sz = 10
+alph = 0.35
 
 # Histogram of STEREO-Ahead/Behind speeds
 def hi_spc_speeds(v,**kwargs):
@@ -63,8 +54,8 @@ def hi_spc_speeds(v,**kwargs):
 	v = np.array(v.astype('float'))
 	plt.hist(v,bins=np.arange(0, max(v) + binwidth, binwidth),color=colors[spc])
 	plt.title("%s STEREO %s" %(tit.upper(),labels[spc]))
-	plt.xlim(speeds_xlim)
-	plt.xlabel(speeds_xlabel)
+	plt.xlim(speeds_lim)
+	plt.xlabel(speeds_label)
 	save(path=os.path.join(config.hicact_path,"%s_speeds_hist_%s" %(tit,labels[spc])),verbose=True)
 
 
@@ -73,66 +64,58 @@ def hicact_speeds(v_a,v_b):
 	plt.hist(v_a,bins=np.arange(0,max(v_a)+binwidth,binwidth),histtype='stepfilled',\
 		normed=False,color='r',label='Ahead')
 	plt.hist(v_b,bins=np.arange(0,max(v_b)+binwidth,binwidth),histtype='stepfilled',\
-		normed=False,color='b',alpha=0.5,label='Behind')
+		normed=False,color='b',alpha=alph,label='Behind')
 	plt.title("HICACTus CME Speeds")
-	plt.xlim(speeds_xlim)
-	plt.xlabel(speeds_xlabel)
+	plt.xlim(speeds_lim)
+	plt.xlabel(speeds_label)
 	plt.ylabel("Count")
 	plt.legend(prop={'size':8})
 	save(path=os.path.join(config.hicact_path,"hicact_speeds_hist"),verbose=True)
 
+
 # Scatterplot of STEREO-Ahead/Behind speeds against position angles
 def hicact_spc_speeds_pa(df_hicact,spc):
-	plt.scatter(df_hicact.v,df_hicact.pa,s=80,facecolor=colors[spc],edgecolor='none',alpha=0.25)
+	plt.scatter(df_hicact.v,df_hicact.pa,s=80,facecolor=colors[spc],edgecolor='none',alpha=alph)
 	#plt.axhline(y=df_hicact_b.pa.min())
 	#plt.axhline(y=df_hicact_b.pa.max())
-	plt.xlim(speeds_xlim)
+	plt.xlim(speeds_lim)
 	plt.title("HICACTus STEREO-%s" %labels[spc])
-	plt.xlabel(speeds_xlabel)
+	plt.xlabel(speeds_label)
 	plt.ylabel("Position Angle ($deg$)")
 	save(path=os.path.join(config.hicact_path,"hicact_speeds_pa_%s" %labels[spc]),verbose=True)
+
 
 # Scatterplot of STEREO-Ahead & Behind speeds against position angles
 def hicact_speeds_pa():
 	plt.figure(num=None,figsize=(6,8),dpi=80,facecolor='w',edgecolor='k')
-	a = plt.scatter(df_hicact_a.v,df_hicact_a.pa,s=20,facecolor='red',edgecolor='none',alpha=0.25)
-	b = plt.scatter(df_hicact_b.v,df_hicact_b.pa,s=20,facecolor='blue',edgecolor='none',alpha=0.25)
-	plt.xlim(speeds_xlim)
+	a = plt.scatter(df_hicact_a.v,df_hicact_a.pa,s=20,facecolor='red',edgecolor='none',alpha=alph)
+	b = plt.scatter(df_hicact_b.v,df_hicact_b.pa,s=20,facecolor='blue',edgecolor='none',alpha=alph)
+	plt.xlim(speeds_lim)
 	plt.ylim([0,360])
 	plt.title("HICACTus STEREO CMEs")
-	plt.xlabel(speeds_xlabel)
+	plt.xlabel(speeds_label)
 	plt.ylabel("Position Angle ($deg$)")
 	plt.legend([a,b],['Ahead','Behind'],prop={'size':8})
 	save(path=os.path.join(config.hicact_path,"hicact_speeds_pa"),verbose=True)
 
-'''
-wp3_speeds=df_hicat[['FP speed [kms-1]','SSE speed [kms-1]','HM speed [kms-1]']]
-wp3_speeds.plot(kind='hist',stacked=True,bins=100)
-save(path=os.path.join(config.hicat_path,"wp3_speeds_hist"),verbose=True)
-'''
 
 # Split speeds by year
-def hicact_speeds_datetime():
+def hicact_speeds_datetime(df_hicact_a,df_hicact_b):
 	import datetime
 	time_format = "%Y/%m/%d %H:%M"
 	datetimes_a = np.array([datetime.datetime.strptime(x,time_format) for x in df_hicact_a.starttime])
 	datetimes_b = np.array([datetime.datetime.strptime(x,time_format) for x in df_hicact_b.starttime])
-	plt.figure(num=None,figsize=(6,8),dpi=80,facecolor='w',edgecolor='k')
-	a = plt.scatter(df_hicact_a.v,datetimes_a,s=20,facecolor='red',edgecolor='none',alpha=0.25)
-	b = plt.scatter(df_hicact_b.v,datetimes_b,s=20,facecolor='blue',edgecolor='none',alpha=0.25)
-	plt.xlim(speeds_xlim)
-	plt.title("HICACTus STEREO CMEs")
-	plt.xlabel(speeds_xlabel)
-	plt.ylabel("Time")
-	plt.legend([a,b],['Ahead','Behind'],prop={'size':8})
+	plt.figure(num=None,figsize=(7,8),dpi=80,facecolor='w',edgecolor='k')
+	a = plt.scatter(datetimes_a,df_hicact_a.v,s=20,facecolor='red',edgecolor='none',alpha=alph)
+	b = plt.scatter(datetimes_b,df_hicact_b.v,s=20,facecolor='blue',edgecolor='none',alpha=alph)
+	plt.ylim([0,2300])
+	plt.title("HICACTus CMEs")
+	plt.ylabel(speeds_label)
+	plt.xlabel("Time")
+	plt.legend([a,b],['Ahead','Behind'],prop={'size':ledge_sz},loc=2)
 	save(path=os.path.join(config.hicact_path,"hicact_speeds_datetimes"),verbose=True)
 
-def fourier_speeds():
-	from scipy.fftpack import fft
-	yf = fft(df_hicact_b.v)
-	plt.scatter(df_hicact_b.v,yf)
-	#plt.show()
-
+# Run all
 def run_all():
 	hi_spc_speeds(df_hicact_a.v,spc=0,tit='hicact')
 	hi_spc_speeds(df_hicact_b.v,spc=1,tit='hicact')
@@ -140,7 +123,7 @@ def run_all():
 	hicact_spc_speeds_pa(df_hicact_a,0)
 	hicact_spc_speeds_pa(df_hicact_b,1)
 	hicact_speeds_pa()
-	hicact_speeds_datetime()
+	hicact_speeds_datetime(df_hicact_a,df_hicact_b)
 
 
 
--- hicat_plots.py
+++ hicat_plots.py
@@ -26,11 +26,7 @@ import config
 from savefig import save
 
 from cmes import hicat
-
 # Call the catalog functions:
-#df_cdaw = cdaw().convert_objects(convert_numeric=True)
-#df_hicact_a = hicact('A').convert_objects(convert_numeric=True)
-#df_hicact_b = hicact('B').convert_objects(convert_numeric=True)
 df_hicat = hicat().convert_objects(convert_numeric=True)
 
 # Drop NaNs
@@ -45,8 +41,9 @@ df_hicat = hicat().convert_objects(convert_numeric=True)
 binwidth=50 
 colors = ['r','b','grey']
 labels = ['Ahead','Behind','']
-speeds_xlim = [0,2100]
-speeds_xlabel = "Speed ($km s^{-1}$)"
+speeds_lim = [0,3000]
+speeds_label = "Speed ($km s^{-1}$)"
+ledge_sz = 10
 
 # Histogram of STEREO-Ahead/Behind speeds
 def hi_geom_speeds(v,**kwargs):
@@ -63,58 +60,78 @@ def hi_geom_speeds(v,**kwargs):
 	v = np.array(v.astype('float'))
 	plt.hist(v,bins=np.arange(0, max(v) + binwidth, binwidth),color=colors[spc])
 	plt.title("%s STEREO %s" %(tit,labels[spc]))
-	#plt.xlim(speeds_xlim)
-	plt.xlabel(speeds_xlabel)
+	#plt.xlim(speeds_lim)
+	plt.xlabel(speeds_label)
 	save(path=os.path.join(config.hicat_path,"%s_speeds_hist_%s" %(tit,labels[spc])),verbose=True)
 
 # Histogram of STEREO-Ahead & Behind speeds
-def hicat_speeds(v_a,v_b,**kwargs):
+def hicat_all_speeds(*args,**kwargs):
+	fp_a = args[0]
+	fp_b = args[1]
+	sse_a = args[2]
+	sse_b = args[3]
+	hm_a = args[4]
+	hm_b = args[5]
 	if 'tit' in kwargs:
 		tit = kwargs['tit']
 	else:
 		tit = ""
-	print max(v_a)
-	print max(v_b)
-	plt.hist(v_a,bins=np.arange(0,max(v_a)+binwidth,binwidth),histtype='stepfilled',\
-		normed=False,color='r',label='Ahead')
-	plt.hist(v_b,bins=np.arange(0,max(v_b)+binwidth,binwidth),histtype='stepfilled',\
-		normed=False,color='b',alpha=0.5,label='Behind')
-	plt.title("HICAT CME Speeds %s" %tit)
-	#plt.xlim(speeds_xlim)
-	plt.xlabel(speeds_xlabel)
+	plt.figure(num=None,figsize=(10,12),dpi=80,facecolor='w',edgecolor='k')
+	plt.subplot(311)
+	plt.hist([fp_a,fp_b],bins=np.arange(0,max(fp_a)+binwidth,binwidth),\
+		stacked=True,normed=False,color=colors[0:2],label=labels[0:2])
+	plt.title("HICAT CME Speeds\n %s" %tit[0])
+	plt.xlim(speeds_lim)
 	plt.ylabel("Count")
-	plt.legend(prop={'size':8})
-	save(path=os.path.join(config.hicat_path,"hicat_speeds_hist"),verbose=True)
+	plt.legend(prop={'size':ledge_sz})
+	plt.subplot(312)
+	plt.hist([sse_a,sse_b],bins=np.arange(0,max(sse_a)+binwidth,binwidth),\
+                stacked=True,normed=False,color=colors[0:2],label=labels[0:2])
+	plt.title("%s" %tit[1])
+        plt.xlim(speeds_lim)
+        plt.ylabel("Count")
+	plt.subplot(313)
+        plt.hist([hm_a,hm_b],bins=np.arange(0,max(hm_a)+binwidth,binwidth),\
+                stacked=True,normed=False,color=colors[0:2],label=labels[0:2])
+	plt.title("%s" %tit[2])
+        plt.xlim(speeds_lim)
+        plt.xlabel(speeds_label)
+        plt.ylabel("Count")
+        save(path=os.path.join(config.hicat_path,"hicat_speeds_hist"),verbose=True)
 
-df_a = df_hicat[(df_hicat['SC']=='A')]
-df_b = df_hicat[(df_hicat['SC']=='B')]
-v_a = df_a[['FP speed [kms-1]']].values
-v_b = df_b[['FP speed [kms-1]']].values
-hicat_speeds(v_a, v_b, tit="FixedPhi")
-#hicat_speeds(df_a[['FP speed [kms-1]']], df_b[['FP speed [kms-1]']], tit="FixedPhi")
 
 # Scatterplot of STEREO-Ahead/Behind speeds against position angles
 def hicat_spc_speeds_pa(df_hicat,spc):
-	plt.scatter(df_hicat.v,df_hicat.pa,s=80,facecolor=colors[spc],edgecolor='none',alpha=0.25)
+	hm = plt.scatter(df_hicat[['HM speed [kms-1]']],df_hicat[['PA-fit']],s=50,facecolor='green',\
+		edgecolor='none',alpha=0.5)
+	sse = plt.scatter(df_hicat[['SSE speed [kms-1]']],df_hicat[['PA-fit']],s=50,facecolor='blue',\
+		edgecolor='none',alpha=0.5)
+	fp = plt.scatter(df_hicat[['FP speed [kms-1]']],df_hicat[['PA-fit']],s=50,facecolor='red',\
+		edgecolor='none',alpha=0.5)
 	#plt.axhline(y=df_hicat_b.pa.min())
 	#plt.axhline(y=df_hicat_b.pa.max())
-	plt.xlim(speeds_xlim)
-	plt.title("HICACTus STEREO-%s" %labels[spc])
-	plt.xlabel(speeds_xlabel)
+	plt.xlim(speeds_lim)
+	plt.title("HICAT STEREO-%s" %labels[spc])
+	plt.xlabel(speeds_label)
 	plt.ylabel("Position Angle ($deg$)")
+	plt.legend([fp,sse,hm],['Fixed-Phi','Self-Similar Exp.','Harmonic Mean'],prop={'size':ledge_sz})
 	save(path=os.path.join(config.hicat_path,"hicat_speeds_pa_%s" %labels[spc]),verbose=True)
 
 # Scatterplot of STEREO-Ahead & Behind speeds against position angles
-def hicat_speeds_pa():
-	plt.figure(num=None,figsize=(6,8),dpi=80,facecolor='w',edgecolor='k')
-	a = plt.scatter(df_hicat_a.v,df_hicat_a.pa,s=20,facecolor='red',edgecolor='none',alpha=0.25)
-	b = plt.scatter(df_hicat_b.v,df_hicat_b.pa,s=20,facecolor='blue',edgecolor='none',alpha=0.25)
-	plt.xlim(speeds_xlim)
+def hicat_speeds_pa(df_hicat):
+	plt.figure(num=None,figsize=(8,10),dpi=80,facecolor='w',edgecolor='k')
+        hm = plt.scatter(df_hicat[['HM speed [kms-1]']],df_hicat[['PA-fit']],s=50,facecolor='green',\
+                edgecolor='none',alpha=0.35)
+        sse = plt.scatter(df_hicat[['SSE speed [kms-1]']],df_hicat[['PA-fit']],s=50,facecolor='blue',\
+                edgecolor='none',alpha=0.35)
+	fp = plt.scatter(df_hicat[['FP speed [kms-1]']],df_hicat[['PA-fit']],s=50,facecolor='red',\
+                edgecolor='none',alpha=0.35)
+	plt.xlim(speeds_lim)
 	plt.ylim([0,360])
-	plt.title("HICACTus STEREO CMEs")
-	plt.xlabel(speeds_xlabel)
+	plt.title("HICAT CMEs")
+	plt.xlabel(speeds_label)
 	plt.ylabel("Position Angle ($deg$)")
-	plt.legend([a,b],['Ahead','Behind'],prop={'size':8})
+	plt.legend([fp,sse,hm],['Fixed-Phi','Self-Similar Exp.','Harmonic Mean'],prop={'size':ledge_sz})
 	save(path=os.path.join(config.hicat_path,"hicat_speeds_pa"),verbose=True)
 
 '''
@@ -124,24 +141,42 @@ save(path=os.path.join(config.hicat_path,"wp3_speeds_hist"),verbose=True)
 '''
 
 # Split speeds by year
-def hicat_speeds_datetime():
+def hicat_speeds_datetime(df_hicat):
 	import datetime
-	time_format = "%Y/%m/%d %H:%M"
-	datetimes_a = np.array([datetime.datetime.strptime(x,time_format) for x in df_hicat_a.starttime])
-	datetimes_b = np.array([datetime.datetime.strptime(x,time_format) for x in df_hicat_b.starttime])
-	plt.figure(num=None,figsize=(6,8),dpi=80,facecolor='w',edgecolor='k')
-	a = plt.scatter(df_hicat_a.v,datetimes_a,s=20,facecolor='red',edgecolor='none',alpha=0.25)
-	b = plt.scatter(df_hicat_b.v,datetimes_b,s=20,facecolor='blue',edgecolor='none',alpha=0.25)
-	plt.xlim(speeds_xlim)
-	plt.title("HICACTus STEREO CMEs")
-	plt.xlabel(speeds_xlabel)
-	plt.ylabel("Time")
-	plt.legend([a,b],['Ahead','Behind'],prop={'size':8})
+	time_format = "%Y-%m-%dT%H:%MZ"
+	datetimes = np.array([datetime.datetime.strptime(x[0],time_format) \
+		for x in df_hicat[['Date [UTC]']].values])
+	plt.figure(num=None,figsize=(8,6),dpi=80,facecolor='w',edgecolor='k')
+	ssz=30
+	hm = plt.scatter(datetimes,df_hicat[['HM speed [kms-1]']],s=ssz,facecolor='green',\
+		edgecolor='none',alpha=0.35)
+	sse = plt.scatter(datetimes,df_hicat[['SSE speed [kms-1]']],s=ssz,facecolor='blue',\
+		edgecolor='none',alpha=0.35)
+	fp = plt.scatter(datetimes,df_hicat[['FP speed [kms-1]']],s=ssz,facecolor='red',\
+		edgecolor='none',alpha=0.35)
+	plt.ylim(speeds_lim)
+	plt.title("HICAT CMEs")
+	plt.ylabel(speeds_label)
+	plt.xlabel("Time")
+	plt.legend([fp,sse,hm],['FP','SSE','HM'],prop={'size':ledge_sz},loc=2)
 	save(path=os.path.join(config.hicat_path,"hicat_speeds_datetimes"),verbose=True)
 
 
 def run_all():
+	df_a = df_hicat[(df_hicat['SC']=='A')]
+	df_b = df_hicat[(df_hicat['SC']=='B')]
+	
 	hi_geom_speeds(df_hicat[["FP speed [kms-1]"]],tit='FixedPhi')
+	
+	hicat_spc_speeds_pa(df_a,0)
+	hicat_spc_speeds_pa(df_b,1)
+	hicat_speeds_pa(df_hicat)
 
+	hicat_all_speeds(df_a[['FP speed [kms-1]']].values, df_b[['FP speed [kms-1]']].values,\
+                df_a[['SSE speed [kms-1]']].values, df_b[['SSE speed [kms-1]']].values,\
+                df_a[['HM speed [kms-1]']].values, df_b[['HM speed [kms-1]']].values,\
+                tit=["Fixed Phi","Self-Similar Expansion","Harmonic Mean"])
+
+	hicat_speeds_datetime(df_hicat)
 
 
